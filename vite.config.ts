9a68d9da264433af025edcff23f2c82e1510ab05
nkoreli <nika.koreli@gmail.com>
2024-10-19 18:19:55 -0700

    add-exclude-to-vite-config-as-temporary-solution

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [svgr({ exportAsDefault: true },), react()],
+
+  server: {
+    port: 4240,
+    host: 'localhost'
+  },
+
+  worker: {
+    format: 'es',
+    plugins: () => [],
+  },
+
   build: {
     // target: "ESNext",
     sourcemap: false,
@@ -17,12 +28,11 @@ export default defineConfig({
     },
     rollupOptions: {
       plugins: [
-        // commonjs(), // Add this plugin
       ],
     },
   },
   optimizeDeps: {
-    include: ['@hinkal/common'],
+    exclude: ['@hinkal/common'], // DO NOT REMOVE THIS LINE: vite does not create "assets" folder in cache, but is trying to read from there: that's why we exclude from cache
     esbuildOptions: {
       tsconfig: './tsconfig.json',
       // Limit target browsers due to issue: Big integer literals are not available in the configured target environment ("chrome87", "edge88", "es2020", "firefox78", "safari13" + 2 overrides)'
