7e8d654876522f52dd19f257495b0b916fad9c11
nkoreli <nika.koreli@gmail.com>
2024-11-27 11:08:34 -0800

    remove-unused

--- vite.config.ts
+++ vite.config.ts
@@ -22,14 +22,9 @@ export default defineConfig({
   },
 
   build: {
-    // target: "ESNext",
     sourcemap: false,
     commonjsOptions: {
       transformMixedEsModules: true
-    },
-    lib: {
-      entry: 'src/main.tsx',
-      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       plugins: [
@@ -37,8 +32,6 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    // exclude: ['@hinkal/common'], // DO NOT REMOVE THIS LINE: vite does not create "assets" folder in cache, but is trying to read from there: that's why we exclude from cache
-    // include: ['blake2b', 'blake-hash', '@axelar-network/axelarjs-sdk', 'libsodium-wrappers', 'ua-parser-js', 'eventemitter3'],
     esbuildOptions: {
       tsconfig: './tsconfig.json',
       // Limit target browsers due to issue: Big integer literals are not available in the configured target environment ("chrome87", "edge88", "es2020", "firefox78", "safari13" + 2 overrides)'
