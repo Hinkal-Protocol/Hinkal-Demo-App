c998341ff2d8f815a7765fe45ca57381bf329a3f
sabaaa1 <sdeli23@freeuni.edu.ge>
2025-12-20 20:13:57 +0400

    fix copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => (
     <div className="flex flex-col">
       <div className="w-full px-[4%] md:px-0 h-[0.5px] bg-[#d6d6d651] my-5" />
       <div className="px-[4%] md:px-0 flex justify-between items-center text-[15px] font-[400] text-[#9E9E9E] pb-6">
-        <span>© 2025 Hinkal. All rights reserved.</span>
+        <span>© 2026 Hinkal. All rights reserved.</span>
       </div>
     </div>
   </div>
