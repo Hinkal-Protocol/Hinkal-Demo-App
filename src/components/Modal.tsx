4ec8b75d104f98700e95fb8f7ec9ee6dd617d0a3
sabaaa1 <sdeli23@freeuni.edu.ge>
2025-12-20 23:06:53 +0400

    Fix modal problem, token choose modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 interface ModalInterface {
   children: ReactNode;
@@ -23,18 +23,29 @@ export const Modal = ({
     {/* this div has onClick so that the popup closes if the user clicks outside of it */}
     <div
       className={`bg-[#000000a6] fixed top-0 left-0 w-full h-screen z-10 ${stylePropsBg} ${
-        isOpen ? 'block' : 'hidden'
+        isOpen ? "block" : "hidden"
       } `}
       onClick={xBtnAction}
     />
     <div
-      className={`fixed w-[90%] bg-black border-[1px] border-solid border-[#4d4d4d] rounded md:w-2/5 left-[5%] top-[20%] md:left-[30%] z-[100] ${styleProps} ${
-        isOpen ? 'opacity-[1] translate-y-[0%] ' : 'opacity-[0] translate-y-[-200%] '
-      } transition-all duration-500 `}
+      className={`fixed w-[90%] bg-black border-[1px] border-solid border-[#4d4d4d] rounded md:w-2/5 left-[5%] top-[20%] md:left-[30%] z-[100]
+    max-h-[80vh] overflow-y-auto
+    ${styleProps}
+    ${
+      isOpen
+        ? "opacity-[1] translate-y-[0%]"
+        : "opacity-[0] translate-y-[-200%]"
+    } transition-all duration-500 `}
     >
       {xBtn && (
-        <button type="button" className="absolute right-2 font-bold cursor-pointer text-white" onClick={xBtnAction}>
-          <i className={`bi bi-x text-[26px] hover:text-[#acaaaa] ${xBtnStyleProps} `} />
+        <button
+          type="button"
+          className="absolute right-2 font-bold cursor-pointer text-white"
+          onClick={xBtnAction}
+        >
+          <i
+            className={`bi bi-x text-[26px] hover:text-[#acaaaa] ${xBtnStyleProps} `}
+          />
         </button>
       )}
 
